64fdf9d001e8214ddc16d66c05744b0ca3053458
TITAN <aluochmarion7@gmail.com>
2024-07-08 12:30:39 +0300

    Models Init

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
-from  flask_sqlachemy import SQLAlchemy
+from  flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData,CheckConstraint
+from sqlalchemy.orm import validates, relationship
 
 
 metadata = MetaData(
@@ -17,32 +18,81 @@ class User (db.Model,SerializerMixin):
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String, nullable=False)
     email=db.Column(db.String, unique=True,nullable=False)
-    adoptions=db.relationship('Adoption',backref='user')
+    adoptions=db.relationship('Adoption',back_populates='user')
+
+    @validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError('Name is required.')
+        return name
+    
+
+    @validates('email')
+    def validate_email(self,key,email):
+        if '@' not in email:
+            raise ValueError('Email address is required')
+        return email
+
+
 
 class Pet(db.Model,SerializerMixin):
     __tablename__='pets'
 
     id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.string,nullable=False)
-    age = db.column(db.Integer,nullable=False)
-    shelter_id=db.Column(db.Integer,db.ForeignKey('shelter.id'))
-    adoptions=db.relationship('Adoption',backred='pet')
+    name=db.Column(db.String,nullable=False)
+    age = db.Column(db.Integer,nullable=False)
+    shelter_id=db.Column(db.Integer,db.ForeignKey('shelters.id'))
+    adoptions=db.relationship('Adoption',back_populates='pet')
+
+    @validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError('Pet name is required.')
+        return name
+    
+    @validates('age')
+    def validate_age(self,key,age):
+        if age is None or age <0:
+            raise ValueError('Pet age must be a positive number.')
+        return age
 
 class Shelter(db.Model,SerializerMixin):
-    __tablename__='shelter'
+    __tablename__='shelters'
 
     id=db.Column(db.Integer, primary_key=True)
-    name=db.column(db.String,nullable=False)
+    name=db.Column(db.String,nullable=False)
     location=db.Column(db.String, nullable=False)
-    pets=db.relationship('Pets', backref='shelter')
+    pets=db.relationship('Pet', back_populates='shelter')
+
+    @validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError('Shelter name is required.')
+        return name
+    
+    @validates('location')
+    def validate_location(self,key,location):
+        if not location:
+            raise ValueError('Location is required.')
+        return location
 
 class Adoption(db.Model,SerializerMixin):
-    __tablename__='adoption'
+    __tablename__='adoptions'
 
     id=db.Column(db.Integer, primary_key=True)
-    user_id=db.Column(db.Integer, db.ForeignKey('user.id', nullable=False))
-    pet_id=db.column(db.Integer, db.ForeignKey('pet.id',nullable=False))
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id', nullable=False))
+    pet_id=db.Column(db.Integer, db.ForeignKey('pets.id',nullable=False))
     adoption_date=db.Column(db.Date,nullable=False)
     comments=db.Column(db.String)
+    
+    user=db.relationship('User', back_populates='adoptions')
+    pet=db.relationship('Pet',back_populates='adoptions')
+
+    @validates('adoption_date')
+    def validates_date(self,key,adoption_date):
+        if not adoption_date:
+            raise ValueError('Adoption date required')
+        return adoption_date
+
 
 
