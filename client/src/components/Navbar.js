f6f3705f3a20271cc3bd583937e5fedfeb2ea3d3
stephenbowen <bowensteven17@gmail.com>
2024-07-09 16:33:24 +0300

    update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light navbar-color">
         <a className="navbar-brand" href="#">
           <Link className='link' to={"/"}><h2 className="ms-3 text-color ">Pawfect Match</h2></Link>
         </a>
