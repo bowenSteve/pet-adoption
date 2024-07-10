25c540f292164db8530aef5945b9b40eb75726e4
Abdulaziz Abdi <amigf76@gmail.com>
2024-07-10 11:34:35 +0300

    update db

--- server/migrations/versions/cac283150fd8_initial_migration.py
+++ server/migrations/versions/cac283150fd8_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration.
 
-Revision ID: 04dce52d2230
+Revision ID: cac283150fd8
 Revises: 
-Create Date: 2024-07-09 12:08:01.656718
+Create Date: 2024-07-10 11:33:18.540833
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '04dce52d2230'
+revision = 'cac283150fd8'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -25,6 +25,8 @@ def upgrade():
     sa.Column('breed', sa.String(), nullable=False),
     sa.Column('age', sa.Integer(), nullable=False),
     sa.Column('location', sa.String(), nullable=True),
+    sa.Column('image_url', sa.String(), nullable=True),
+    sa.Column('description', sa.String(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('users',
@@ -39,7 +41,6 @@ def upgrade():
     sa.Column('user_id', sa.Integer(), nullable=True),
     sa.Column('pet_id', sa.Integer(), nullable=True),
     sa.Column('adoption_date', sa.String(), nullable=True),
-    sa.Column('comments', sa.String(), nullable=True),
     sa.ForeignKeyConstraint(['pet_id'], ['pets.id'], name=op.f('fk_adoptions_pet_id_pets')),
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], name=op.f('fk_adoptions_user_id_users')),
     sa.PrimaryKeyConstraint('id')
